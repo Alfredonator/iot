ba2b429c4052529695898aaae85f333f59b0ed7c
hvsio <h4sio@o2.pl>
2021-03-16 15:38:56 +0100

    b

--- scripts/start.bash
+++ scripts/start.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 master_ip="10.42.0.49"
-hostname=`hostname`
-vars="export ROS_MASTER_URI=\"http://${master_ip}:11311\";"
-vars=$vars" export ROS_HOSTNAME=\"${hostname}.local\";"
-vars=$vars" export PS1=\"\[\033[00;33m\][\${ROS_MASTER_URI}]\[\033[00m\] \${PS1}\""
-bash --rcfile <(cat ~/.bashrc; echo "${vars}")
+#hostname=`hostname`
+#vars="export ROS_MASTER_URI=\"http://${master_ip}:11311\";"
+#vars=$vars" export ROS_HOSTNAME=\"${hostname}.local\";"
+#vars=$vars" export PS1=\"\[\033[00;33m\][\${ROS_MASTER_URI}]\[\033[00m\] \${PS1}\""
+#bash --rcfile <(cat ~/.bashrc; echo "${vars}")
--- scripts/unbreak.bash
+++ scripts/unbreak.bash
@@ -1,2 +1,8 @@
 #!/bin/bash
+master_ip="10.42.0.49"
+hostname=`hostname`
+vars="export ROS_MASTER_URI=\"http://${master_ip}:11311\";"
+vars=$vars" export ROS_HOSTNAME=\"${hostname}.local\";"
+vars=$vars" export PS1=\"\[\033[00;33m\][\${ROS_MASTER_URI}]\[\033[00m\] \${PS1}\""
+bash --rcfile <(cat ~/.bashrc; echo "${vars}")
 rostopic pub bridge_jnt_reset edo_core_msgs/JointReset "{joints_mask: 63, disengage_steps: 2000, disengage_offset: 3.5}"
