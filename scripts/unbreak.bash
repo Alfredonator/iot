786daf5008e2cf79c5ad686035ac6155d348c406
hvsio <h4sio@o2.pl>
2021-03-16 15:39:38 +0100

    ye

--- scripts/unbreak.bash
+++ scripts/unbreak.bash
@@ -6,3 +6,4 @@ vars=$vars" export ROS_HOSTNAME=\"${hostname}.local\";"
 vars=$vars" export PS1=\"\[\033[00;33m\][\${ROS_MASTER_URI}]\[\033[00m\] \${PS1}\""
 bash --rcfile <(cat ~/.bashrc; echo "${vars}")
 rostopic pub bridge_jnt_reset edo_core_msgs/JointReset "{joints_mask: 63, disengage_steps: 2000, disengage_offset: 3.5}"
+echo curva
