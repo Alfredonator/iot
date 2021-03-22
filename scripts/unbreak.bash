c67291548b102079f52a934a4304e73e4e09fbc1
Alicja <alicjaharaszczuk@gmail.com>
2021-03-22 12:26:17 +0100

    scripts

--- scripts/unbreak.bash
+++ scripts/unbreak.bash
@@ -1,9 +1,18 @@
 #!/bin/bash
+
+
 master_ip="10.42.0.49"
 hostname=`hostname`
-vars="export ROS_MASTER_URI=\"http://${master_ip}:11311\";"
-vars=$vars" export ROS_HOSTNAME=\"${hostname}.local\";"
-vars=$vars" export PS1=\"\[\033[00;33m\][\${ROS_MASTER_URI}]\[\033[00m\] \${PS1}\""
-bash --rcfile <(cat ~/.bashrc; echo "${vars}")
-rostopic pub bridge_jnt_reset edo_core_msgs/JointReset "{joints_mask: 63, disengage_steps: 2000, disengage_offset: 3.5}"
-echo curva
+
+export ROS_MASTER_URI=http://${master_ip}:11311
+export ROS_HOSTNAME=${hostname}.local
+
+
+
+
+
+
+source ~/edo_ws/devel/setup.bash
+
+rostopic pub bridge_jnt_reset edo_core_msgs/JointReset "{joints_mask: 63, disengage_steps: 2000, disengage_offset: 3.5}" -1
+
